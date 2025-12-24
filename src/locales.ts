dc8ba4a1961e4f7fba2e0ee6ccc31fa6123d6165
mazelabsjp <cjamestait@gmail.com>
2025-12-25 06:15:25 +0900

    initial commit for astro i18n starter

--- src/locales.ts
+++ src/locales.ts
@@ -20,12 +20,4 @@ export const LOCALES_SETTING: LocaleSetting = {
 	ja: {
 		label: "日本語",
 	},
-	"zh-cn": {
-		label: "简体中文",
-		lang: "zh-CN",
-	},
-	ar: {
-		label: "العربية",
-		dir: "rtl",
-	},
 };
